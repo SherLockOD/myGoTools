993fc58acc10f9ba298df45f1d5f165aafb44a69
likuo <likuo@ifeng.com>
2019-05-28 10:24:39 +0800

    Add: httpbin origin ip address

--- httpbin/httpbin.go
+++ httpbin/httpbin.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"net/http"
+	"log"
 	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"strings"
 )
 
 type HeaderBody struct {
-	Args    map[string]string `json:"args"`
-	Headers map[string]string `json:"headers"`
-	Origin  string            `json:"origin"`
-	Url     string            `json:"url"`
+	Args 	map[string]string 	`json:"args"`
+	Headers map[string]string 	`json:"headers"`
+	Origin 	string 				`json:"origin"`
+	Url 	string 				`json:"url"`
 }
 
 var Port = "4000"
@@ -20,18 +20,27 @@ var Port = "4000"
 func handler(w http.ResponseWriter, r *http.Request) {
 	headers := make(map[string]string)
 	args := make(map[string]string)
-	headersBody := HeaderBody{Origin: "", Url: "http://" + r.Host + r.URL.String()}
+
+	// headersBody
+	origin := strings.Split(r.RemoteAddr, ":")[0]
+	headersBody := HeaderBody{Origin: origin, Url: "http://" + r.Host + r.URL.String()}
+
+	// headers
+	Host := strings.ReplaceAll(r.Host, ":" + Port, "")
+	headers["Host"] = Host
+
 	for k, v := range r.Header {
 		headers[k] = v[0]
 	}
-	Host := strings.ReplaceAll(r.Host, ":"+Port, "")
-	headers["Host"] = Host
+
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
+
 	for k, v := range r.Form {
 		args[k] = v[0]
 	}
+	
 	headersBody.Args = args
 	headersBody.Headers = headers
 	b, err := json.Marshal(headersBody)
@@ -44,5 +53,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:"+Port, nil))
+	log.Fatal(http.ListenAndServe("localhost:" + Port, nil))
 }
